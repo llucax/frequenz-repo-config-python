be587a510b38f297daec172dcb2f7406f69a1587
Mathias L. Baumann <mathias.baumann@frequenz.com>
2024-05-07 17:01:28 +0200

    Refactor code to use the interface properly
    Whatever it was before, it worked more by accident than intention.
    
    Signed-off-by: Mathias L. Baumann <mathias.baumann@frequenz.com>

--- src/frequenz/repo/config/pytest/examples.py
+++ src/frequenz/repo/config/pytest/examples.py
@@ -31,7 +31,8 @@ from typing import Any
 
 from sybil import Example
 from sybil.evaluators.python import pad
-from sybil.parsers.myst import CodeBlockParser
+from sybil.parsers.myst import CodeBlockParser, PythonCodeBlockParser
+from sybil.typing import Evaluator
 
 _PYLINT_DISABLE_COMMENT = (
     "# pylint: {}=unused-import,wildcard-import,unused-wildcard-import"
@@ -44,22 +45,6 @@ _FORMAT_STRING = """
 {enable_pylint}
 
 {code}"""
-
-
-def get_sybil_arguments() -> dict[str, Any]:
-    """Get the arguments to pass when instantiating the Sybil object to lint docs examples.
-
-    Returns:
-        The arguments to pass when instantiating the Sybil object.
-    """
-    return {
-        "parsers": [_CustomPythonCodeBlockParser()],
-        "patterns": ["*.py"],
-        # This is a hack because Sybil seems to have issues with `__init__.py` files.
-        # See https://github.com/frequenz-floss/frequenz-repo-config-python/issues/113
-        # for details
-        "excludes": ["__init__.py"],
-    }
 
 
 def _get_import_statements(code: str) -> list[str]:
@@ -134,9 +119,9 @@ class _CustomPythonCodeBlockParser(CodeBlockParser):
     Pylint warnings which are unimportant for code examples are disabled.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, language: str | None = None, _: Evaluator | None = None) -> None:
         """Initialize the parser."""
-        super().__init__("python")
+        super().__init__(language, self.evaluate)
 
     def evaluate(self, example: Example) -> None | str:
         """Validate the extracted code example using pylint.
@@ -234,3 +219,27 @@ def _validate_with_pylint(
         return output.splitlines()
 
     return []
+
+
+class MyPythonCodeBlockParser(PythonCodeBlockParser):
+    """Custom Python code block parser that uses the custom code block parser."""
+
+    codeblock_parser_class: type[_CustomPythonCodeBlockParser] = (
+        _CustomPythonCodeBlockParser
+    )
+
+
+def get_sybil_arguments() -> dict[str, Any]:
+    """Get the arguments to pass when instantiating the Sybil object to lint docs examples.
+
+    Returns:
+        The arguments to pass when instantiating the Sybil object.
+    """
+    return {
+        "parsers": [MyPythonCodeBlockParser()],
+        "patterns": ["*.py"],
+        # This is a hack because Sybil seems to have issues with `__init__.py` files.
+        # See https://github.com/frequenz-floss/frequenz-repo-config-python/issues/113
+        # for details
+        "excludes": ["__init__.py"],
+    }
